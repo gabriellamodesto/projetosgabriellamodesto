f235b81e760f090b0c50eb71b026f4056719f28a
gabriellamodesto <gabimodesto2010@gmail.com>
2026-03-30 13:58:09 -0400

    Atualização de condicionais e listagem

--- Projetos-Python/cadastro_e_analise.py
+++ Projetos-Python/cadastro_e_analise.py
@@ -1,5 +1,6 @@
 dados_fixos = ("Curso: Introdução à Programação", "Duração: 4 semanas", "Carga horária: 40 horas", "Nível: Iniciante")
 opcao = -1
+participantes = []
 
 print(f"{dados_fixos[0]} | {dados_fixos[2]} | {dados_fixos[3]}")
 while opcao != 4:
@@ -12,11 +13,18 @@ while opcao != 4:
 
     if opcao == 1:
         nome = input("Digite o nome do participante: ")
-        idade = int(input("Digite a idade do participante: "))
-        horas_estudo = int(input("Digite a quantidade de horas de estudo: "))
-        area_interesse = input("Digite a área de interesse: ")
         
-        participantes = []
+        idade = int(input("Digite a idade do participante: "))
+        while idade < 0:
+            print("Idade inválida, tente novamente.")
+            idade = int(input("Digite a idade do participante: "))
+        
+        horas_estudo = int(input("Digite a quantidade de horas de estudo: "))
+        while horas_estudo < 0:
+            print("Quantidade de horas de estudo inválida, tente novamente.")
+            horas_estudo = int(input("Digite a quantidade de horas de estudo: "))
+        
+        area_interesse = input("Digite a área de interesse: ")
         
         participantes.append({
             'nome': nome,
@@ -32,8 +40,8 @@ while opcao != 4:
         if len(participantes) == 0:
             print("Nenhum participante cadastrado.")
         else:
-            for i, aluno in enumerate(participantes):
-                print(f"{i + 1}. {aluno['nome']} | Idade: {aluno['idade']} | Horas de Estudo: {aluno['horas_estudo']} | Área de Interesse: {aluno['area_interesse']}")
+            for i, aluno in enumerate(participantes, start=1):
+                print(f"{i}. {aluno['nome']} | Idade: {aluno['idade']} | Horas de Estudo: {aluno['horas_estudo']} | Área de Interesse: {aluno['area_interesse']}")
     
     elif opcao == 3:
         print("Análise dos participantes:")
@@ -54,10 +62,21 @@ while opcao != 4:
         print(f"Quantidade de participantes maiores de idade: {maiores_idade}")
         print(f"Média de horas de estudo: {media_horas_estudo:.2f}")
             
+        print("Participantes com bom desempenho de estudo: ")
         for aluno in participantes:
             if aluno['horas_estudo'] >= 5:
-                print(f"Participante {aluno['nome']} tem um bom desempenho.")
-    
+                print(f"{aluno['nome']} - Horas de Estudo: {aluno['horas_estudo']}")
+        
+        print("Áreas de interesse dos participantes: ")
+
+        areas = []
+        for aluno in participantes:
+            if aluno['area_interesse'] not in areas:
+                areas.append(aluno['area_interesse'])
+                
+        for area in areas:
+            print(f"- {area}")
+
     elif opcao == 4:
         print("Saindo do sistema. Até mais!")
     
